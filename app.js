2db17d6e0394e8ac0535f07645207f5b4a28fdc0
Mehdi39 <jahid4k@gmail.com>
2021-02-09 23:28:54 +0600

    again push

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 
 // Getting all the element by id
 const searchBtn = document.getElementById('enter-btn');
-const mealList = document.getElementById('');
+const mealList = document.getElementById('menu-item-list');
+const mealDetailsContent = document.querySelector('.meal-details-content');
 
 //  Adding all event listeners
 searchBtn.addEventListener('click', getMealList);
+mealList.addEventListener('click', mealDetails);
 
 
 function getMealList() {
     // getting the value of searchbar and removing the any white space using trim method.
     let searchInputTxt = document.getElementById('searchBar').value.trim();
-
-    
-
-    // passing the search input text as dynamic api call.
+    if (searchInputTxt == '') {
+        alert("Likha pora na korle kaj hobe na! kisu khaite hoile likhte hoibe");
+    } else {
+        // passing the search input text as dynamic api call.
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInputTxt}`)
         .then(res => res.json())
         .then(data => displayMenu(data));
+    }
 }
 
 
@@ -31,24 +34,24 @@ const displayMenu = data => {
             mealBox = mealBox + `
 
                 
-                    <div class="col-2 meal-box">
-                        <div class ="meal-item" data-id ="${meal.idMeal}">
-                            <div class = "meal-img">
-                                <img src = "${meal.strMealThumb}" alt = "food">
-                            </div>
-                            <div class = "meal-name">
-                                <h3>${meal.strMeal}</h3>
-                                <a href = "#" class = "recipe-btn btn btn-warning">Get Recipe</a>
-                            </div>
+                <div class="col-2 meal-box">
+                    <div class ="meal-item" data-id ="${meal.idMeal}">
+                        <div class = "meal-img">
+                            <img src = "${meal.strMealThumb}" alt = "food">
+                        </div>
+                        <div class = "meal-name">
+                            <h3>${meal.strMeal}</h3>
+                            <a href = "#" class = "recipe-btn btn btn-warning">Get Recipe</a>
                         </div>
                     </div>
-                
-                    
+                </div>      
             `;
         })
+        mealDiv.classList.remove('notFound');
     } else {
         // if not match with a meal then show eror.
-        mealBox = "Meal not found! try again";
+        mealBox = `<h1>Meal Not Found! try again</h1>`
+        mealDiv.classList.add('notFound');
     }
     // passing all mealbox into mealDiv.
     mealDiv.innerHTML = mealBox;
@@ -56,8 +59,33 @@ const displayMenu = data => {
 
 
 // section for meal details
-function mealDetails() {
-    console.log('gg');
+function mealDetails(meal) {
+    if (meal.target.classList.contains('recipe-btn')) {
+        let selectedMeal = meal.target.parentElement.parentElement;
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${selectedMeal.dataset.id}`)
+        .then(res => res.json())
+        .then(data => dispalyMealDetails(data.meals));
+    }
+}
+
+function dispalyMealDetails(meal) {
+    meal = meal[0];
+    let mealBox = `
+        <h2 class = "recipe-title">${meal.strMeal}</h2>
+        <p class = "recipe-category">${meal.strCategory}</p>
+        <div class = "recipe-instruct">
+            <h3>Instructions:</h3>
+            <p>${meal.strInstructions}</p>
+        </div>
+        <div class = "recipe-meal-img">
+            <img src = "${meal.strMealThumb}" alt = "">
+        </div>
+        <div class = "recipe-link">
+            <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
+        </div>
+    `;
+    mealDetailsContent.innerHTML = mealBox;
+    mealDetailsContent.parentElement.classList.add('showRecipe');
 }
 
 
