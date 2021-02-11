43443b72f728d6a1a8630f38b7ab37832c387ace
Jahid <jahid4k@gmail.com>
2021-02-11 19:04:16 +0600

    motamoti final

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ function getMealList() {
 
 // Method or function for Displaying all the matching meals.
 const displayMenu = data => {
+    // console.log(data.meals[0]);
     const mealDiv = document.getElementById('menu-item-list');
     let mealBox = "";
 
@@ -32,16 +33,13 @@ const displayMenu = data => {
         data.meals.forEach(meal => {
             // mealbox will add forEach meal in the array/object.
             mealBox = mealBox + `
-
-                
-                <div class="col-2 meal-box">
+                <div class="col-2 meal-box mealDetails">
                     <div class ="meal-item" data-id ="${meal.idMeal}">
                         <div class = "meal-img">
-                            <img src = "${meal.strMealThumb}" alt = "food">
+                            <img class="recipe-btn" src = "${meal.strMealThumb}" alt = "food">
                         </div>
                         <div class = "meal-name">
-                            <h3>${meal.strMeal}</h3>
-                            <a href = "#" class = "recipe-btn btn btn-warning">Get Recipe</a>
+                            <h3 class="recipe-btn">${meal.strMeal}</h3>
                         </div>
                     </div>
                 </div>      
@@ -68,60 +66,36 @@ function mealDetails(meal) {
     }
 }
 
+let mealBox = ``
+
 function dispalyMealDetails(meal) {
     meal = meal[0];
-    let mealBox = `
-        <h2 class = "recipe-title">${meal.strMeal}</h2>
-        <p class = "recipe-category">${meal.strCategory}</p>
-        <div class = "recipe-instruct">
-            <h3>Instructions:</h3>
-            <p>${meal.strInstructions}</p>
-        </div>
+    const ingredients = [];
+    for (let i = 1; i <= 20; i++) {
+      if (meal[`strIngredient${i}`]) {
+        ingredients.push(
+          `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]
+          }`
+        );
+      } else {
+        break;
+      }
+    }
+
+    
+    mealBox = `
         <div class = "recipe-meal-img">
             <img src = "${meal.strMealThumb}" alt = "">
         </div>
-        <div class = "recipe-link">
-            <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
+        <h2 class = "recipe-title">${meal.strMeal}</h2>
+        
+        <div class = "recipe-instruct">
+            <h3>Ingredients:</h3>
+            <ul>
+  ${ingredients.map(ing => `<li>${ing}</li>`).join('')}
+  </ul>
         </div>
     `;
     mealDetailsContent.innerHTML = mealBox;
     mealDetailsContent.parentElement.classList.add('showRecipe');
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${menu}`
-// fetch(url)
-// .then(res => res.json())
-// .then(data => displayMenu(data));
-
-// const displayMenuasdf = data => {
-//     console.log(data);
-// }
-
-// const searchBar = document.getElementById('searchBar');
-
-// searchBar.addEventListener('keyup', (e) => {
-//     console.log(e.target.value);
-// });
-
-
-// const showResult = name => {
-//     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata`;
-//     fetch(url)
-//     .then(res => res.json())
-//     .then(data => displayMenu(data));
-// }
-
-
-
